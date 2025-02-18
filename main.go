b8fe2dbe042ede1e48fb2291edb654632d66d061
muharromi42 <muharromi42@gmail.com>
2025-02-18 23:55:44 +0700

    update method

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	router.POST("/posts", controllers.CreatePost)
 	router.GET("/posts", controllers.FindPosts)
 	router.GET("/posts/:id", controllers.FindPost)
+	router.PATCH("/posts/:id", controllers.UpdatePost)
 
 	router.Run(":3050")
 }
